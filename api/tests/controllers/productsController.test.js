7d4db3f4476a8b650c32a2b0c2e9fb92e8d8fe5e
Jakub Urban <iakub.urban@gmail.com>
2018-11-15 23:54:33 +0000

    stuff

--- api/tests/controllers/productsController.test.js
+++ api/tests/controllers/productsController.test.js
@@ -1,13 +1,48 @@
+const sinon = require('sinon');
 const expect = require('chai').expect;
 
+const Products = require('../../db/sequelize').Products;
+const ProductModel = require('../../db/models/product');
 const ProductsController = require('../../controllers/productsController');
 
 describe('ProductsController.get_all()', () => {
-    it('should return all available products', () => {
+
+    let sandbox;
+
+    beforeEach((done) => {
+        sandbox = sinon.createSandbox();
+
+        Products.destroy({
+            where: {},
+            truncate: true
+        });
+        done();
+    });
+
+    afterEach((done) => {
+        sandbox.restore();
+        done();
+    });
+
+    it('should return all available products', (done) => {
 
         // ARRANGE 
-        
+        var product1Mock = sinon.mock(ProductModel);
+        var product2Mock = sinon.mock(ProductModel);
 
+        var expectedResult = [product1Mock, product2Mock];           
+        Products.bulkCreate(expectedResult)
+            .then(() => {
+                // ACT 
+                ProductsController.get_all()
+            })
+            .then((req, res) => {
 
+                console.log(res);
+                //ASSERT
+                // expect(res.Products).to.equal(expectedResult);
+                // expect(res.Total).to.equal(expect.length);
+                done();
+            });
     });
 });
