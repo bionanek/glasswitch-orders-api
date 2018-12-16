c4ba32e3ff2a660555f7c4256a4a65252bf13f16
Jakub Urban <iakub.urban@gmail.com>
2018-12-16 09:12:57 +0000

    define customerDataModel and register it in sequelize.

--- api/db/dbHelper.js
+++ api/db/dbHelper.js
@@ -2,6 +2,8 @@ const config = require('config');
 const Sequelize = require('sequelize');
 var PricesDataModel = require('./models/priceDataModel').priceDataModel;
 var ProductsDataModel = require('./models/productDataModel').productDataModel;
+var CustomersDataModel = require('./models/customerDataModel').customerDataModel;
+
 let sequelize;
 
 function getSequelize() {
@@ -18,12 +20,14 @@ sequelize = getSequelize();
 
 var Prices = sequelize.define('prices', PricesDataModel, {freezeTableName: true})
 var Products = sequelize.define('products', ProductsDataModel, {freezeTableName: true});
+var Customers = sequelize.define('customers', CustomersDataModel, {freezeTableName: true});
 
 sequelize.sync({ force: true });
 
 
 exports.Prices = Prices;
 exports.Products = Products;
+exports.Customers = Customers;
 exports.Sequelize = sequelize;
 
 
