0c86c26b637cb0e81a6e44b4e22e7e4f2d2cdb70
elvixpix <oskarkaczmarzyk@pm.me>
2019-03-02 17:15:18 +0100

    removed try catch blocks from Repository. converted Promise approach to Async in Delete.

--- api/db/repositories/productRepository.js
+++ api/db/repositories/productRepository.js
@@ -6,77 +6,50 @@ const { Validate } = require('@repos/validators/validators');
 exports.Products = Products;
 
 exports.createProduct = async (productData) => {
-    try {
-        return await Products.create(productData, { include: [Products.Price], as: 'productId'});
-    } catch (error) {
-        throw new Error(error);
-    }
+    return await Products.create(productData, { include: [Products.Price], as: 'productId'});
 };
 
 exports.updateProduct = async (productId, updatedProductData) => {
-    try {
-        const requestedProduct = await Products.findById(productId);
+    const requestedProduct = await Products.findById(productId);
 
-        Validate.ValidateIdExists(requestedProduct);
+    Validate.ValidateIdExists(requestedProduct);
 
-        const productUpdate = await Products.update(updatedProductData, 
-            { where: { id: productId } });
-        const priceUpdate = await Prices.update(updatedProductData.price, 
-            { where: { id: updatedProductData.priceId } });
+    const productUpdate = await Products.update(updatedProductData, 
+        { where: { id: productId } });
+    const priceUpdate = await Prices.update(updatedProductData.price, 
+        { where: { id: updatedProductData.priceId } });
 
-        return parseInt(productUpdate) + parseInt(priceUpdate);
-    } catch (error) {
-        throw new Error(error);
-    }
+    return parseInt(productUpdate) + parseInt(priceUpdate);
 };
 
 exports.deleteProduct = async (productId) => {
-    try {
-        const requestedProduct = await Products.findById(productId);
+    const requestedProduct = await Products.findById(productId);
 
-        Validate.ValidateIdExists(requestedProduct);
+    Validate.ValidateIdExists(requestedProduct);
 
-        let affectedRows = 0;
-        let priceId;
+    let affectedRows = 0; 
+    const priceId = requestedProduct.priceId;
 
-        return await Products.findById(productId)
-            .then((product) => {
-                priceId = product.priceId;
-                return Products.destroy({ where: { id: productId }, cascade: true });
-            })
-            .then((removedRows) => {
-                affectedRows += removedRows;
-                return Prices.destroy({ where: { id: priceId } });            
-            })
-            .then((removedRows) => {
-                affectedRows += removedRows;
-                return affectedRows;
-            })
-    } catch (error) {
-        throw new Error(error);
-    }
+    affectedRows += await Products.destroy(
+        { where: { id: productId }, cascade: true });
+    affectedRows += await Prices.destroy(
+        { where: { id: priceId } });
+
+    return affectedRows;
 };
 
 exports.getAll = async () => {
-    try {
-        const allProducts = await Products.findAll({ include: [Products.Price] })
-            .map(el => el.get({ plain: true }));
+    const allProducts = await Products.findAll({ include: [Products.Price] })
+        .map(el => el.get({ plain: true }));
 
-        return allProducts;
-    } catch (error) {
-        throw new Error(error);
-    }
+    return allProducts;
 };
 
 exports.getById = async (productId) => {
-    try {
-        const requestedProduct = await Products.findById(productId, 
-            { include: [Products.Price] });
+    const requestedProduct = await Products.findById(productId, 
+        { include: [Products.Price] });
 
-        Validate.ValidateIdExists(requestedProduct);
+    Validate.ValidateIdExists(requestedProduct);
 
-        return requestedProduct;
-    } catch (error) {
-        throw new Error(error);
-    }
+    return requestedProduct;
 };
