2f6bfb221383f7baf956545b06ef492ac439c263
elvixpix <oskarkaczmarzyk@pm.me>
2019-03-05 17:26:09 +0100

    removed unnecessary try catch blocks from priceRepo.

--- api/db/repositories/priceRepository.js
+++ api/db/repositories/priceRepository.js
@@ -3,20 +3,10 @@ const Prices = require('@db/dbHelper').Prices;
 exports.Prices = Prices;
 
 exports.createPrice = async (priceData) => {
-    try {
-        return await Prices.create(priceData)
-    } catch (error) {
-        throw new Error(error);
-    }
+    return await Prices.create(priceData)
 };
 
-exports.getAll = () => {
-    try {
-        const requestedPrices = await Prices.findAll()
-            .map(el => el.get({ plain: true }))
-
-        return requestedPrices;
-    } catch (error) {
-        throw new Error(error);
-    }
+exports.getAll = async () => {
+    return await Prices.findAll()
+        .map(el => el.get({ plain: true }));
 };
