7d007d0d4c708844c9084d4477f3abcc2ae39532
elvixpix <oskarkaczmarzyk@pm.me>
2019-03-08 21:55:09 +0100

    IdVerification into IdExists.

--- api/db/repositories/verification/verificate.js
+++ api/db/repositories/verification/verificate.js
@@ -1,7 +1,7 @@
 const { IdNotFound } = require('../../../helpers/errors');
 
 class Verificate {
-    static IdVerification(requestedItem) {
+    static IdExists(requestedItem) {
         if (requestedItem === null || requestedItem === undefined) {
             throw new IdNotFound('Given ID doesn\'t exists.');
         }
