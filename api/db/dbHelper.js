a817a87fa884b9f6d90ddd513e319d2b482a31b9
elvixpix <oskarkaczmarzyk@pm.me>
2019-03-09 22:09:38 +0100

    initialized Order belongsTo Customer and Customer hasMany Orders with sequelize. initialized order in dbHelper.

--- api/db/dbHelper.js
+++ api/db/dbHelper.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 var PricesDataModel = require('@models/priceDataModel').priceDataModel;
 var ProductsDataModel = require('@models/productDataModel').productDataModel;
 var CustomersDataModel = require('@models/customerDataModel').customerDataModel;
+var OrdersDataModel = require('@models/orderDataModel').orderDataModel;
 
 let sequelize;
 
@@ -21,15 +22,16 @@ sequelize = getSequelize();
 var Products = sequelize.define('product', ProductsDataModel, {freezeTableName: true});
 var Prices = sequelize.define('price', PricesDataModel, {freezeTableName: true})
 var Customers = sequelize.define('customer', CustomersDataModel, {freezeTableName: true});
+var Orders = sequelize.define('order', OrdersDataModel, {freezeTableName: true});
 
 Products.Price = Products.belongsTo(Prices, { onDelete: 'cascade' });
+Orders.Customers = Orders.belongsTo(Customers, { onDelete: 'cascade' });
+Customers.Orders = Customers.hasMany(Orders, { as: 'Orders' });
 
 sequelize.sync({ force: true });
 
 exports.Prices = Prices;
 exports.Products = Products;
 exports.Customers = Customers;
+exports.Orders = Orders;
 exports.Sequelize = sequelize;
-
-
-
---
+++ api/db/models/orderDataModel.js
@@ -0,0 +1,9 @@
+const Types = require('sequelize');
+
+exports.orderDataModel = {
+    id: {
+        type: Types.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+}
