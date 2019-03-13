76a921db136c6cecaf4e5e4dddef2a6f7d62e95b
elvixpix <oskarkaczmarzyk@pm.me>
2019-03-14 00:52:48 +0100

    setup sequelize assocation between orders and products.

--- api/db/dbHelper.js
+++ api/db/dbHelper.js
@@ -22,16 +22,14 @@ sequelize = getSequelize();
 var Products = sequelize.define('product', ProductsDataModel, {freezeTableName: true});
 var Prices = sequelize.define('price', PricesDataModel, {freezeTableName: true})
 var Customers = sequelize.define('customer', CustomersDataModel, {freezeTableName: true});
-var Orders = sequelize.define('orders', OrdersDataModel, {freezeTableName: true});
+var Orders = sequelize.define('order', OrdersDataModel, {freezeTableName: true});
 
 Products.Price = Products.belongsTo(Prices, { onDelete: 'cascade' });
 Orders.Customers = Orders.belongsTo(Customers, { onDelete: 'cascade' });
-Products.Orders = Orders.belongsTo(Products, { onDelete: 'cascade' });
-
-// Belongs-To-Many associations?
-
-// Products.Orders = Products.hasMany(Orders, { as: 'Products' });
 Customers.Orders = Customers.hasMany(Orders, { as: 'Orders' });
+
+Products.belongsToMany(Orders, {through: 'Products_Orders', foreignKey: 'Products_rowId'});
+Orders.belongsToMany(Products, {through: 'Products_Orders', foreignKey: 'Orders_rowId'});
 
 sequelize.sync({ force: true });
 
