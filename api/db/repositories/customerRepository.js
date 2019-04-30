4e99b0b356b73b78c8a07664213bfcbaebbbd531
@elvixpix <oskarkaczmarzyk@pm.me>
2019-04-30 16:01:12 +0200

    get customers with filters

--- api/db/repositories/customerRepository.js
+++ api/db/repositories/customerRepository.js
@@ -1,25 +1,52 @@
-const Customers = require('@db/dbHelper').Customers;
+const { Customers } = require('@db/dbHelper')
+const { Sequelize } = require('@db/dbHelper')
 
-exports.Customers = Customers;
+const Op = Sequelize.Op
+exports.Customers = Customers
 
 exports.createCustomer = async (customerData) => {
-    return await Customers.create(customerData);
-};
+    return Customers.create(customerData)
+}
 
 exports.updateCustomer = async (customerId, updatedCustomerData) => {
     return Customers.update(updatedCustomerData, 
-        { where: { id: customerId } });
-};
+        { where: { id: customerId } })
+}
 
 exports.deleteCustomer = async (customerId) => {
     return Customers.destroy(
-        { where: { id: customerId }, cascade: true });
-};
+        { where: { id: customerId }, cascade: true })
+}
 
 exports.getAll = async () => {
-    return await Customers.findAll();
-};
+    return Customers.findAll()
+}
 
 exports.getById = async (customerId) => {
-    return await Customers.findById(customerId);
-};
+    return Customers.findById(customerId)
+}
+
+exports.getSearchResults = async (searchPhrase) => {
+    const likeOperator = { [Op.like]: `%${searchPhrase}%`}
+
+    const searchResults = await Customers.findAll({
+        where: {
+            [Op.or]: [
+                { name: likeOperator },
+                { email: likeOperator },
+                { phone: likeOperator },
+                { vatNumber: likeOperator },
+                { delivery_street: likeOperator },
+                { delivery_city: likeOperator },
+                { delivery_country: likeOperator },
+                { delivery_postCode: likeOperator },
+                { billing_street: likeOperator },
+                { billing_city: likeOperator },
+                { billing_country: likeOperator },
+                { billing_postCode: likeOperator }
+            ]
+        }
+    })
+
+    return searchResults
+}
--- api/routes/customersRouter.js
+++ api/routes/customersRouter.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const router = express.Router();
-const { CustomerValidation } = require('@validation/customerValidation');
+const express = require('express')
+const router = express.Router()
+const { CustomerValidation } = require('@validation/customerValidation')
 
-const CustomersController = require('@controllers/customersController');
+const CustomersController = require('@controllers/customersController')
 
-router.get('/', CustomersController.getAll);
-router.get('/:customerId', CustomerValidation.Validate, CustomersController.getById);
-router.post('/', CustomerValidation.Validate, CustomersController.create);
-router.patch('/:customerId', CustomerValidation.Validate, CustomersController.update);
-router.delete('/:customerId', CustomerValidation.Validate, CustomersController.delete);
+router.get('/', CustomersController.getAll)
+router.get('/search', CustomerValidation.Validate, CustomersController.getSearchResults)
+router.get('/:customerId', CustomerValidation.Validate, CustomersController.getById)
+router.post('/', CustomerValidation.Validate, CustomersController.create)
+router.patch('/:customerId', CustomerValidation.Validate, CustomersController.update)
+router.delete('/:customerId', CustomerValidation.Validate, CustomersController.delete)
 
-module.exports = router;
+module.exports = router
