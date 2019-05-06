cea210c0d26d44d763d5ed053bc4546b46182f04
Oskar Kaczmarzyk <oskarkaczmarzyk@pm.me>
2019-05-06 22:46:54 +0200

    Feature/orders (#74)
    * order unit test. need to add order data model.
    
    * order validation. need to add order data model.
    
    * initialized Order belongsTo Customer and Customer hasMany Orders with sequelize. initialized order in dbHelper.
    
    * orderDataModel, sequelize associations within orders, customers and products. WIP
    
    * order model fixed.
    
    * setup sequelize assocation between orders and products.
    
    * validatecreate body
    
    * finished order validation.
    
    * fixed ValidateUpdate.
    
    * fixed ValidateUpdate. v2
    
    * max currency length, date type
    
    * max currency length refactored.
    
    * fixed ifs within validate update.
    
    * removed unnecessary if from update.
    
    * finished unit tests for orders.
    
    * Feature/order_endpoints (#72)
    
    * added @routes alias, orderRoutes for the app, ordersRoutes, Controller, Domain. WIP
    
    * fixed validation for orderCreate. changed Orders.Customers into Orders.Customer for sequelize. order is being created.
    
    * changed associations for orders-products for now. verification if requested customer exists. fixed validation for boolean types.
    
    * orders getAll. productsCount fix.
    
    * ID verification for orders. orders getById.
    
    * addProduct for the order. associations orders - products. orders GetAll & GetById. orders verification.
    
    * associations orders-products. update & delete order. add product with quantity into order.
    
    * created counters for productsCount and productsTotalPrice by selected currency while creating an order. removed banch of unneccessary semicolons. removed unneccessary validation.
    
    * endpoints for delete product from an order & change quantity of product in an order
    
    * fixed counters while adding and removing product from an order.
    
    * add cors package, add app.use(cors()), change api port
    
    * refactor 1.counters for order && 2.verifications if requested id exists, now it gives more specific info what hasnt been found. create order with products array. validation that you cant change productsCount, productsTotalPrice and customerId with an orderUpdate.
    
    * addProducts into an order within updateOrder. you cannot add product that exists in requested order
    
    * adding new product into an order and change quantity within orderUpdate
    
    * wip - simultaneously adding product into order and changing currency of a whole order
    
    * fixed changing currency for the order and adding or changing quantity of products in the same request
    
    * removed unneccessary price endpoints
    
    * get customers with filters
    
    * removed unneccessary asyncs
    
    * removed unused asyncs
    
    * execute single responsibility within orders. wip -> changing currency, productsTotalPrice makes unwanted magic while deleting products from an order
    
    * change currency and/or add a product & change quantity of a product in an order
    
    * refactored adding products & deleting products from an order
    
    * refactored domain to avoid getting products inside orderHelper
    
    * orderDomain refactor
    
    * small corrections

--- api/db/dbHelper.js
+++ api/db/dbHelper.js
@@ -1,35 +1,43 @@
-const config = require('config');
-const Sequelize = require('sequelize');
-var PricesDataModel = require('@models/priceDataModel').priceDataModel;
-var ProductsDataModel = require('@models/productDataModel').productDataModel;
-var CustomersDataModel = require('@models/customerDataModel').customerDataModel;
+const config = require('config')
+const Sequelize = require('sequelize')
 
-let sequelize;
+var PricesDataModel = require('@models/priceDataModel').priceDataModel
+var ProductsDataModel = require('@models/productDataModel').productDataModel
+var CustomersDataModel = require('@models/customerDataModel').customerDataModel
+var OrdersDataModel = require('@models/orderDataModel').orderDataModel
+
+let sequelize
 
 function getSequelize() {
     if (sequelize) {
-        return sequelize;
+        return sequelize
     }
-
-    const options = config.dbOptions;
-
-    return new Sequelize(options);
+    
+    const options = config.dbOptions
+    return new Sequelize(options)
 }
 
-sequelize = getSequelize();
+sequelize = getSequelize()
 
-var Products = sequelize.define('product', ProductsDataModel, {freezeTableName: true});
-var Prices = sequelize.define('price', PricesDataModel, {freezeTableName: true})
-var Customers = sequelize.define('customer', CustomersDataModel, {freezeTableName: true});
+var Products = sequelize.define('product', ProductsDataModel, { freezeTableName: true })
+var Prices = sequelize.define('price', PricesDataModel, { freezeTableName: true })
+var Customers = sequelize.define('customer', CustomersDataModel, { freezeTableName: true })
+var Orders = sequelize.define('order', OrdersDataModel, { freezeTableName: true })
+var ProductsOrders = sequelize.define('products_orders', { quantity: Sequelize.INTEGER })
 
-Products.Price = Products.belongsTo(Prices, { onDelete: 'cascade' });
+Products.Price = Products.belongsTo(Prices, { onDelete: 'cascade' })
+Orders.Customer = Orders.belongsTo(Customers, { onDelete: 'cascade' })
+Customers.Order = Customers.hasMany(Orders, { as: 'orders' })
 
-sequelize.sync({ force: true });
+Orders.belongsToMany(Products, { as: 'products', through: ProductsOrders, foreignKey: 'orderId' })
+Products.belongsToMany(Orders, { as: 'orders', through: ProductsOrders, foreignKey: 'productId' })
 
-exports.Prices = Prices;
-exports.Products = Products;
-exports.Customers = Customers;
-exports.Sequelize = sequelize;
+sequelize.sync({ force: true })
 
+exports.Prices = Prices
+exports.Products = Products
+exports.Customers = Customers
+exports.Orders = Orders
+exports.ProductsOrders = ProductsOrders
 
-
+exports.Sequelize = sequelize
---
+++ api/routes/validation/orderValidation.js
@@ -0,0 +1,82 @@
+const {
+    RequestValidationError,
+    ArgumentIsIncorrectType,
+    UpdateError
+} = require('@helpers/errors')
+
+class OrderValidation {
+    static Validate(request, response, next) {
+        try {
+            switch (request.method) {
+                case "POST":
+                    OrderValidation.ValidateCreate(request.body)
+                    break
+                case "GET":
+                    OrderValidation.ValidateIdIsNaN(request.params.orderId)
+                    break
+                case "PATCH":
+                    OrderValidation.ValidateUpdate(request)
+                    break
+                case "DELETE":
+                    OrderValidation.ValidateIdIsNaN(request.params.orderId)
+                    break
+            }
+            next()
+        } catch (error) {
+            return response.status(error.code).json(error)
+        }
+    }
+
+    static ValidateCreate(orderData) {
+        OrderValidation.ValidateAllFieldsUndefined(orderData)
+        OrderValidation.ValidateAllFieldsEmpty(orderData)
+    }
+
+    static ValidateAllFieldsUndefined(orderData) {
+        if (!orderData.currency
+            || !orderData.email
+            || !orderData.deadline
+            || !orderData.customerId
+            || orderData.confirmationSent === undefined
+            || orderData.proformaSent     === undefined
+            || orderData.invoiceSent      === undefined
+            || orderData.settledPayment   === undefined) {
+                throw new RequestValidationError('One or more request fields are missing.')
+        }
+    }
+
+    static ValidateAllFieldsEmpty(orderData) {
+        if (!/\S/.test(orderData.currency)
+            || !/\S/.test(orderData.email)
+            || !/\S/.test(orderData.deadline)) {
+                throw new RequestValidationError('One or more request fields are empty.')
+        }
+    }
+
+    static ValidateIdIsNaN(id) {
+        if (isNaN(id)) {
+            throw new ArgumentIsIncorrectType('Order ID must be an integer.')
+        }
+    }
+
+    static ValidateUpdate(request) {
+        OrderValidation.ValidateIdIsNaN(request.params.orderId)
+        const updatedOrderData = request.body
+
+        if (updatedOrderData.productsCount
+            || updatedOrderData.productsTotalPrice
+            || updatedOrderData.customerId) {
+                throw new UpdateError('You cannot change three props manually! These are customerId, productsCount & productsTotalPrice.')
+        }
+
+        if (!/\S/.test(updatedOrderData.currency)
+            || !/\S/.test(updatedOrderData.email)
+            || !/\S/.test(updatedOrderData.deadline)) {
+                throw new UpdateError('One or more request fields are empty.')
+        }
+    }
+}
+
+module.exports = {
+    OrderValidation
+}
