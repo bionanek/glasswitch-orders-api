90a0b277fe265981347f69cf1111b874922076ea
Jakub Urban <iakub.urban@gmail.com>
2019-05-19 21:48:17 +0100

    add new endpoint for getting products by price range

--- api/domain/productsDomain.js
+++ api/domain/productsDomain.js
@@ -1,56 +1,68 @@
-const productRepo = require('@repos/productRepository');
-const { IdNotFound, SequelizeError } = require('@helpers/errors');
-const { Verification, Resources } = require('@verify/verification');
+const productRepo = require("@repos/productRepository");
+const { IdNotFound, SequelizeError } = require("@helpers/errors");
+const { Verification, Resources } = require("@verify/verification");
 
-exports.create = async (productData) => {
-    try {
-        return await productRepo.createProduct(productData);
-    } catch (error) {
-        throw new Error(error);
-    }
+exports.create = async productData => {
+	try {
+		return await productRepo.createProduct(productData);
+	} catch (error) {
+		throw new Error(error);
+	}
 };
 
 exports.update = async (productId, updatedProductData) => {
-    try {
-        await Verification.IdExists(Resources.Products, productId);
+	try {
+		await Verification.IdExists(Resources.Products, productId);
 
-        return await productRepo.updateProduct(productId, updatedProductData);
-    } catch (error) {
-        if (error.name.includes("Sequelize")) {
-            throw new SequelizeError('Field cannot be null.');
-        }
-        throw new IdNotFound('Product with given ID doesn\'t exist. No product was updated.');
-    }
+		return await productRepo.updateProduct(productId, updatedProductData);
+	} catch (error) {
+		if (error.name.includes("Sequelize")) {
+			throw new SequelizeError("Field cannot be null.");
+		}
+		throw new IdNotFound(
+			"Product with given ID doesn't exist. No product was updated."
+		);
+	}
 };
 
-exports.delete = async (productId) => {
-    try {
-        await Verification.IdExists(Resources.Products, productId);
+exports.delete = async productId => {
+	try {
+		await Verification.IdExists(Resources.Products, productId);
 
-        return await productRepo.deleteProduct(productId);
-    } catch (error) {
-        throw new IdNotFound('Product with given ID doesn\'t exist. No product was deleted.');
-    }
+		return await productRepo.deleteProduct(productId);
+	} catch (error) {
+		throw new IdNotFound(
+			"Product with given ID doesn't exist. No product was deleted."
+		);
+	}
 };
 
 exports.getAll = async () => {
-    try {
-        return await productRepo.getAll();
-    } catch (error) {
-        throw new Error('No products were found.')
-    }
+	try {
+		return await productRepo.getAll();
+	} catch (error) {
+		throw new Error("No products were found.");
+	}
 };
 
-exports.getById = async (productId) => {
-    try {
-        await Verification.IdExists(Resources.Products, productId);
-        
-        return await productRepo.getById(productId);
-    } catch (error) {
-        throw new IdNotFound('Product with given ID doesn\'t exist.');
-    }
+exports.getById = async productId => {
+	try {
+		await Verification.IdExists(Resources.Products, productId);
+
+		return await productRepo.getById(productId);
+	} catch (error) {
+		throw new IdNotFound("Product with given ID doesn't exist.");
+	}
 };
 
-exports.getSearchResults = async (searchPhrase) => {
-    return await productRepo.getSearchResults(searchPhrase);
+exports.getSearchResults = async searchPhrase => {
+	return await productRepo.getSearchResults(searchPhrase);
+};
+
+exports.getByPriceRange = async (priceRange, order) => {
+	try {
+		return await productRepo.getByPriceRange(priceRange, order);
+	} catch (ex) {
+		throw new Error(ex);
+	}
 };
--- api/routes/productsRouter.js
+++ api/routes/productsRouter.js
@@ -1,14 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { ProductValidation } = require('@validation/productValidation');
+const { ProductValidation } = require("@validation/productValidation");
 
-const ProductsController = require('@controllers/productsController');
+const ProductsController = require("@controllers/productsController");
 
-router.get('/', ProductsController.getAll);
-router.get('/search', ProductValidation.Validate, ProductsController.getSearchResults);
-router.get('/:productId', ProductValidation.Validate, ProductsController.getById);
-router.post('/', ProductValidation.Validate, ProductsController.create);
-router.patch('/:productId', ProductValidation.Validate, ProductsController.update);
-router.delete('/:productId', ProductValidation.Validate, ProductsController.delete);
+router.get("/", ProductsController.getAll);
+router.get(
+	"/search",
+	ProductValidation.Validate,
+	ProductsController.getSearchResults
+);
+router.get(
+	"/:productId",
+	ProductValidation.Validate,
+	ProductsController.getById
+);
+router.get("/by/price", ProductsController.getByPriceRange);
+router.post("/", ProductValidation.Validate, ProductsController.create);
+router.patch(
+	"/:productId",
+	ProductValidation.Validate,
+	ProductsController.update
+);
+router.delete(
+	"/:productId",
+	ProductValidation.Validate,
+	ProductsController.delete
+);
 
 module.exports = router;
