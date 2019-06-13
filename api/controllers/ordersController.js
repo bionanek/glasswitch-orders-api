a46eb07ec0b6e6c8fe4b7dc86775a6940bab8e1d
@elvixpix <oskarkaczmarzyk@pm.me>
2019-06-13 15:56:33 +0200

    fixed server response after creating an order

--- api/controllers/ordersController.js
+++ api/controllers/ordersController.js
@@ -1,66 +1,69 @@
-const ordersDomain = require('@domains/ordersDomain')
+const ordersDomain = require("@domains/ordersDomain")
 
 exports.create = async (request, response, next) => {
-    try {
-        const orderData = request.body
-        const order = await ordersDomain.create(orderData)
+	try {
+		const orderData = request.body
+		const order = await ordersDomain.create(orderData)
 
-        response.status(200).json(order)
-    } catch (error) {
-        response.status(error.code).json(error)
-    }
+		response.status(200).json(order)
+	} catch (error) {
+		response.status(error.code).json(error)
+	}
 }
 
 exports.update = async (request, response, next) => {
-    try {
-        const updatedOrderData = request.body
-        const orderId = request.params.orderId
-        const affectedRows = await ordersDomain.update(orderId, updatedOrderData)
+	try {
+		const updatedOrderData = request.body
+		const orderId = request.params.orderId
+		const affectedRows = await ordersDomain.update(orderId, updatedOrderData)
 
-        response.status(200).json(affectedRows)
-    } catch (error) {
-        response.status(error.code).json(error)
-    }
+		response.status(200).json(affectedRows)
+	} catch (error) {
+		response.status(error.code).json(error)
+	}
 }
 
 exports.delete = async (request, response, next) => {
-    try {
-        const orderId = request.params.orderId
-        const affectedRows = await ordersDomain.delete(orderId)
-        
-        response.status(200).json(affectedRows)
-    } catch (error) {
-        response.status(error.code).json(error)
-    }
+	try {
+		const orderId = request.params.orderId
+		const affectedRows = await ordersDomain.delete(orderId)
+
+		response.status(200).json(affectedRows)
+	} catch (error) {
+		response.status(error.code).json(error)
+	}
 }
 
 exports.getAll = async (request, response, next) => {
-    try {
-        const orders = await ordersDomain.getAll()
+	try {
+		const orders = await ordersDomain.getAll()
 
-        response.status(200).json(orders)
-    } catch (error) {
-        response.status(error.code).json(error)
-    }
+		response.status(200).json(orders)
+	} catch (error) {
+		response.status(error.code).json(error)
+	}
 }
 
 exports.getById = async (request, response, next) => {
-    try {
-        const order = await ordersDomain.getById(request.params.orderId)
+	try {
+		const order = await ordersDomain.getById(request.params.orderId)
 
-        response.status(200).json(order)
-    } catch (error) {
-        response.status(error.code).json(error)
-    }
+		response.status(200).json(order)
+	} catch (error) {
+		response.status(error.code).json(error)
+	}
 }
 
 exports.deleteProducts = async (request, response, next) => {
-    try {
-        const orderId = request.params.orderId
-        const products = await ordersDomain.deleteProducts(orderId, request.body.products)
+	try {
+		const orderId = request.params.orderId
+		const products = await ordersDomain.deleteProducts(
+			orderId,
+			request.body.products
+		)
 
-        response.status(200).json(products)
-    } catch (error) {
-        response.status(error.code).json(error)
-    }
+		response.status(200).json(products)
+	} catch (error) {
+		response.status(error.code).json(error)
+	}
 }
--- api/domain/ordersDomain.js
+++ api/domain/ordersDomain.js
@@ -1,191 +1,245 @@
-const orderRepo = require('@repos/orderRepository')
-const productRepo = require('@repos/productRepository')
-const { Verification, Resources } = require('@verify/verification')
-const { IdNotFound, SequelizeError } = require('@helpers/errors')
-const { OrderHelpers } = require('@repos/orderHelpers/orderHelpers')
+const orderRepo = require("@repos/orderRepository")
+const productRepo = require("@repos/productRepository")
+const { Verification, Resources } = require("@verify/verification")
+const { IdNotFound, SequelizeError } = require("@helpers/errors")
+const { OrderHelpers } = require("@repos/orderHelpers/orderHelpers")
 
-exports.create = async (orderData) => {
-    try {
-        await Verification.IdExists(Resources.Customers, orderData.customerId)
+exports.create = async orderData => {
+	try {
+		await Verification.IdExists(Resources.Customers, orderData.customerId)
 
-        for (let n = 0; n < orderData.wantedProducts.length; n++) 
-            await Verification.IdExists(Resources.Products, orderData.wantedProducts[n].id)
-    
-        const order = await orderRepo.createOrder(orderData)
+		for (let n = 0; n < orderData.wantedProducts.length; n++)
+			await Verification.IdExists(
+				Resources.Products,
+				orderData.wantedProducts[n].id
+			)
 
-        return this.addProducts(order, orderData.wantedProducts) 
-    } catch (error) {
-        throw new IdNotFound(error.message + ' && Order has not been placed!')
-    }
+		const order = await orderRepo.createOrder(orderData)
+
+		await this.addProducts(order, orderData.wantedProducts)
+
+		return orderRepo.getById(order.id)
+	} catch (error) {
+		throw new IdNotFound(error.message + " && Order has not been placed!")
+	}
 }
 
 exports.update = async (orderId, updatedOrderData) => {
-    try {
-        await Verification.IdExists(Resources.Orders, orderId)
-        const updatedProducts = updatedOrderData.updatedProducts
-        const order = await orderRepo.getById(orderId)
+	try {
+		await Verification.IdExists(Resources.Orders, orderId)
+		const updatedProducts = updatedOrderData.updatedProducts
+		const order = await orderRepo.getById(orderId)
 
-        await this.updateProducts(order, updatedProducts)
+		await this.updateProducts(order, updatedProducts)
 
-        await orderRepo.updateOrder(orderId, updatedOrderData)
-        let updatedOrder = await orderRepo.getById(orderId)
+		await orderRepo.updateOrder(orderId, updatedOrderData)
+		let updatedOrder = await orderRepo.getById(orderId)
 
-        if (order.currency !== updatedOrderData.currency)
-            await this.changeCurrency(order, updatedOrder)
-        
-        return orderRepo.updateOrder(orderId, updatedOrderData)
-    } catch (error) {
-        if (error.name.includes("Sequelize")) {
-            throw new SequelizeError('Field cannot be null.')
-        }
-        throw new IdNotFound(error.message + ' && None order was updated.')
-    }
+		if (order.currency !== updatedOrderData.currency)
+			await this.changeCurrency(order, updatedOrder)
+
+		return orderRepo.updateOrder(orderId, updatedOrderData)
+	} catch (error) {
+		if (error.name.includes("Sequelize")) {
+			throw new SequelizeError("Field cannot be null.")
+		}
+		throw new IdNotFound(error.message + " && None order was updated.")
+	}
 }
 
-exports.delete = async (orderId) => {
-    try {
-        await Verification.IdExists(Resources.Orders, orderId)
-        
-        return orderRepo.deleteOrder(orderId)
-    } catch (error) {
-        throw new IdNotFound(error.message + ' && No order was deleted.')
-    }
+exports.delete = async orderId => {
+	try {
+		await Verification.IdExists(Resources.Orders, orderId)
+
+		return orderRepo.deleteOrder(orderId)
+	} catch (error) {
+		throw new IdNotFound(error.message + " && No order was deleted.")
+	}
 }
 
 exports.getAll = () => {
-    try {
-        return orderRepo.getAll()
-    } catch (error) {
-        throw new Error('No orders were found.')
-    }
+	try {
+		return orderRepo.getAll()
+	} catch (error) {
+		throw new Error("No orders were found.")
+	}
 }
 
-exports.getById = async (orderId) => {
-    try {
-        await Verification.IdExists(Resources.Orders, orderId)
+exports.getById = async orderId => {
+	try {
+		await Verification.IdExists(Resources.Orders, orderId)
 
-        return orderRepo.getById(orderId)
-    } catch (error) {
-        throw new IdNotFound(error.message)
-    }
+		return orderRepo.getById(orderId)
+	} catch (error) {
+		throw new IdNotFound(error.message)
+	}
 }
 
 exports.addProducts = async (order, productsArray) => {
-    try {
-        for (let n = 0; n < productsArray.length; n++) {
-            const productInOrder = await order.getProducts({ where: { id: productsArray[n].id } })
+	try {
+		for (let n = 0; n < productsArray.length; n++) {
+			const productInOrder = await order.getProducts({
+				where: { id: productsArray[n].id }
+			})
 
-            if (productInOrder.length !== 0) continue
-            const product = await productRepo.getById(productsArray[n].id)
-            
-            await OrderHelpers.addQuantityAndTotalPrice(order, product, productsArray[n].quantity)
-            await orderRepo.updateOrder(order.id, order.dataValues)
-            orderRepo.addProducts(order, productsArray[n].id, productsArray[n].quantity)
-        }
-    } catch (error) {
-        throw new IdNotFound(error.message)
-    }
+			if (productInOrder.length !== 0) continue
+			const product = await productRepo.getById(productsArray[n].id)
+
+			await OrderHelpers.addQuantityAndTotalPrice(
+				order,
+				product,
+				productsArray[n].quantity
+			)
+			await orderRepo.updateOrder(order.id, order.dataValues)
+			await orderRepo.addProducts(
+				order,
+				productsArray[n].id,
+				productsArray[n].quantity
+			)
+		}
+	} catch (error) {
+		throw new IdNotFound(error.message)
+	}
 }
 
 exports.deleteProducts = async (orderId, productsArray) => {
-    try {
-        const order = await orderRepo.getById(orderId)
+	try {
+		const order = await orderRepo.getById(orderId)
 
-        for (let n = 0; n < productsArray.length; n++) {
-            const productInOrder = await order.getProducts({ where: { id: productsArray[n].id } })
-            
-            if (productInOrder.length === 0) continue
-            const productToDelete = await productRepo.getById(productsArray[n].id)
-            const quantity = productInOrder[0].products_orders.quantity
+		for (let n = 0; n < productsArray.length; n++) {
+			const productInOrder = await order.getProducts({
+				where: { id: productsArray[n].id }
+			})
 
-            await OrderHelpers.subtractQuantityAndTotalPrice(order, productToDelete, quantity)
-            await orderRepo.updateOrder(order.id, order.dataValues)
-            orderRepo.deleteProducts(order, productsArray[n].id)
-        }
-    } catch (error) {
-        throw new IdNotFound(error.message)
-    }
+			if (productInOrder.length === 0) continue
+			const productToDelete = await productRepo.getById(productsArray[n].id)
+			const quantity = productInOrder[0].products_orders.quantity
+
+			await OrderHelpers.subtractQuantityAndTotalPrice(
+				order,
+				productToDelete,
+				quantity
+			)
+			await orderRepo.updateOrder(order.id, order.dataValues)
+			orderRepo.deleteProducts(order, productsArray[n].id)
+		}
+	} catch (error) {
+		throw new IdNotFound(error.message)
+	}
 }
 
 exports.changeQuantity = async (order, products) => {
-    try {
-        for (let n = 0; n < products.length; n++) {
-            const productInOrder = await order.getProducts({ where: { id: products[n].id } })
+	try {
+		for (let n = 0; n < products.length; n++) {
+			const productInOrder = await order.getProducts({
+				where: { id: products[n].id }
+			})
 
-            if (productInOrder.length === 0) continue
+			if (productInOrder.length === 0) continue
 
-                await this.deleteProducts(order.id, products)
-                const updatedOrder = await orderRepo.getById(order.id)
-                this.addProducts(updatedOrder, products)
-        }
-    } catch (error) {
-        throw new IdNotFound(error.message)
-    }
+			await this.deleteProducts(order.id, products)
+			const updatedOrder = await orderRepo.getById(order.id)
+			this.addProducts(updatedOrder, products)
+		}
+	} catch (error) {
+		throw new IdNotFound(error.message)
+	}
 }
 
 exports.changeCurrency = async (prevOrder, updatedOrder) => {
-    const productsToUpdate = updatedOrder.products
+	const productsToUpdate = updatedOrder.products
 
-    await this.deleteProductsOldCurrency(prevOrder, updatedOrder, productsToUpdate)
-    await this.addProductsNewCurrency(updatedOrder, productsToUpdate)
+	await this.deleteProductsOldCurrency(
+		prevOrder,
+		updatedOrder,
+		productsToUpdate
+	)
+	await this.addProductsNewCurrency(updatedOrder, productsToUpdate)
 }
 
-exports.deleteProductsOldCurrency = async (prevOrder, updatedOrder, productsToUpdate) => {
-    for (let n = 0; n < productsToUpdate.length; n++) {
-        const productInOrder = await prevOrder.getProducts({ where: { id: productsToUpdate[n].id } })
-        const product = await productRepo.getById(productsToUpdate[n].id)
-        const prevQuantity = productInOrder[0].products_orders.quantity
+exports.deleteProductsOldCurrency = async (
+	prevOrder,
+	updatedOrder,
+	productsToUpdate
+) => {
+	for (let n = 0; n < productsToUpdate.length; n++) {
+		const productInOrder = await prevOrder.getProducts({
+			where: { id: productsToUpdate[n].id }
+		})
+		const product = await productRepo.getById(productsToUpdate[n].id)
+		const prevQuantity = productInOrder[0].products_orders.quantity
 
-        await OrderHelpers.subtractQuantityAndTotalPrice(prevOrder, product, prevQuantity)
+		await OrderHelpers.subtractQuantityAndTotalPrice(
+			prevOrder,
+			product,
+			prevQuantity
+		)
 
-        updatedOrder.dataValues.productsCount = prevOrder.dataValues.productsCount
-        updatedOrder.dataValues.productsTotalPrice = prevOrder.dataValues.productsTotalPrice
+		updatedOrder.dataValues.productsCount = prevOrder.dataValues.productsCount
+		updatedOrder.dataValues.productsTotalPrice =
+			prevOrder.dataValues.productsTotalPrice
 
-        await orderRepo.updateOrder(updatedOrder.id, updatedOrder.dataValues)
-        await orderRepo.deleteProducts(updatedOrder, productsToUpdate[n].id)
-    }
+		await orderRepo.updateOrder(updatedOrder.id, updatedOrder.dataValues)
+		await orderRepo.deleteProducts(updatedOrder, productsToUpdate[n].id)
+	}
 }
 
 exports.addProductsNewCurrency = async (updatedOrder, productsToUpdate) => {
-    for (let n = 0; n < updatedOrder.products.length; n++) {
-        const product = await productRepo.getById(productsToUpdate[n].id)
-        const newQuantity = productsToUpdate[n].products_orders.quantity
+	for (let n = 0; n < updatedOrder.products.length; n++) {
+		const product = await productRepo.getById(productsToUpdate[n].id)
+		const newQuantity = productsToUpdate[n].products_orders.quantity
 
-        await OrderHelpers.addQuantityAndTotalPrice(updatedOrder, product, newQuantity)
-        await orderRepo.updateOrder(updatedOrder.id, updatedOrder.dataValues)
-        orderRepo.addProducts(updatedOrder, product.id, newQuantity)
-    }
+		await OrderHelpers.addQuantityAndTotalPrice(
+			updatedOrder,
+			product,
+			newQuantity
+		)
+		await orderRepo.updateOrder(updatedOrder.id, updatedOrder.dataValues)
+		orderRepo.addProducts(updatedOrder, product.id, newQuantity)
+	}
 }
 
 exports.updateProducts = async (order, productsArray) => {
-    for (let n = 0; n < productsArray.length; n++) {
-        const productInOrder = await order.getProducts({ where: { id: productsArray[n].id } })
-        const product = await productRepo.getById(productsArray[n].id)
-        const newQuantity = productsArray[n].quantity
+	for (let n = 0; n < productsArray.length; n++) {
+		const productInOrder = await order.getProducts({
+			where: { id: productsArray[n].id }
+		})
+		const product = await productRepo.getById(productsArray[n].id)
+		const newQuantity = productsArray[n].quantity
 
-        await Verification.IdExists(Resources.Products, product.id)
+		await Verification.IdExists(Resources.Products, product.id)
 
-        if (productInOrder.length === 0) {
-            await this.updateOrderNewProducts(order, product, newQuantity)
-        } else {
-            const prevQuantity = productInOrder[0].products_orders.quantity
-            await this.updateOrderQuantityUpdate(order, product, prevQuantity, newQuantity)
-        }
-    }
+		if (productInOrder.length === 0) {
+			await this.updateOrderNewProducts(order, product, newQuantity)
+		} else {
+			const prevQuantity = productInOrder[0].products_orders.quantity
+			await this.updateOrderQuantityUpdate(
+				order,
+				product,
+				prevQuantity,
+				newQuantity
+			)
+		}
+	}
 }
 
 exports.updateOrderNewProducts = async (order, product, quantity) => {
-    await OrderHelpers.addQuantityAndTotalPrice(order, product, quantity)
-    await orderRepo.updateOrder(order.id, order.dataValues)
-    orderRepo.addProducts(order, product.id, quantity)
+	await OrderHelpers.addQuantityAndTotalPrice(order, product, quantity)
+	await orderRepo.updateOrder(order.id, order.dataValues)
+	orderRepo.addProducts(order, product.id, quantity)
 }
 
-exports.updateOrderQuantityUpdate = async (order, product, prevQuantity, newQuantity) => {
-    await OrderHelpers.subtractQuantityAndTotalPrice(order, product, prevQuantity)
-    await orderRepo.updateOrder(order.id, order.dataValues)
-    await orderRepo.deleteProducts(order, product.id)
+exports.updateOrderQuantityUpdate = async (
+	order,
+	product,
+	prevQuantity,
+	newQuantity
+) => {
+	await OrderHelpers.subtractQuantityAndTotalPrice(order, product, prevQuantity)
+	await orderRepo.updateOrder(order.id, order.dataValues)
+	await orderRepo.deleteProducts(order, product.id)
 
-    await OrderHelpers.addQuantityAndTotalPrice(order, product, newQuantity)
-    await orderRepo.updateOrder(order.id, order.dataValues)
-    orderRepo.addProducts(order, product.id, newQuantity)
+	await OrderHelpers.addQuantityAndTotalPrice(order, product, newQuantity)
+	await orderRepo.updateOrder(order.id, order.dataValues)
+	orderRepo.addProducts(order, product.id, newQuantity)
 }
